963cf7a84c42eab7a5456c20caef62fad6f516e0
Jonas Degrave <jonasdegrave@poli.ufrj.br>
2022-08-22 21:38:51 -0300

    Python Flask HTML Server

---
+++ python-flask-server/swagger_server/models/__init__.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+
+# flake8: noqa
+from __future__ import absolute_import
+# import models into model package
+from swagger_server.models.alias_body import AliasBody
+from swagger_server.models.event import Event
+from swagger_server.models.profile_body import ProfileBody
+from swagger_server.models.track_body import TrackBody
