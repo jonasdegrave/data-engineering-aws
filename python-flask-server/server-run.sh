764a000e1adbadb546b46a0919daf3bd3e6b4eae
Jonas Degrave <jonasdegrave@poli.ufrj.br>
2022-08-24 14:43:21 -0300

    Updated EC2 server-run.sh script to pull git changes

--- python-flask-server/server-run.sh
+++ python-flask-server/server-run.sh
@@ -1,6 +1,13 @@
 # Pull changes from Git
 git fetch --all
-git pull
+
+# WARNING: This discards all local changes!
+# (for production EC2 only, not for dev)
+git reset --hard origin/master
+
+# Make scripts executable
+chmod u+x server-config.sh
+chmod u+x server-run.sh
 
 # Run Flask HTTP server
 python3 -m swagger_server
