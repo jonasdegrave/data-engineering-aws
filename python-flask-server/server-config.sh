47c322ee02f2f5791a2554e469a913b8318dfec5
Jonas Degrave <jonasdegrave@poli.ufrj.br>
2022-08-24 15:02:27 -0300

    Correctly sets awscli and aws configure into server-config.sh

--- python-flask-server/server-config.sh
+++ python-flask-server/server-config.sh
@@ -1,5 +1,16 @@
 # Install PIP
-sudo apt install python3-pip
+sudo apt install python3-pip -y
+
+# Install AWS CLI
+sudo apt install awscli -y
+
+# Perform 'aws configure' automatically setting these fields correctly:
+aws configure set region eu-central-1
+aws configure set output table
+
+# TODO: Configure this correctly into EC2 ambient variables:
+#aws configure set aws_access_key_id     $AWS_ACCESS_KEY_ID
+#aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
 
 # Install AWS SDK for Python (boto3)
 pip3 install boto3
