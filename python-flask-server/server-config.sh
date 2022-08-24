ecb91a8d77b6bb61551ecb3a02d80207541c07af
Jonas Degrave <jonasdegrave@poli.ufrj.br>
2022-08-24 13:01:03 -0300

    Add boto3 install to config script

--- python-flask-server/server-config.sh
+++ python-flask-server/server-config.sh
@@ -1,5 +1,8 @@
 # Install PIP
 sudo apt install python3-pip
 
-# Install the requirements
+# Install AWS SDK for Python (boto3)
+pip3 install boto3
+
+# Install the requirements for Flask
 pip3 install -r requirements.txt
